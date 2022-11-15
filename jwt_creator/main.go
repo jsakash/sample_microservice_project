9c1a3415e7a807ea9df7551810bd2fab4bf96f78
Akash JS <akashjs710@gmail.com>
2022-11-15 15:17:20 +0530

    jwt creation complete

--- jwt_creator/main.go
+++ jwt_creator/main.go
@@ -1,1 +1,50 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+var MySigningKey = []byte(os.Getenv("SECRET_KEY"))
+
+func GetJWT() (string, error) {
+	token := jwt.New(jwt.SigningMethodHS256)
+	claims := token.Claims.(jwt.MapClaims)
+
+	claims["authorized"] = true
+	claims["client"] = "akashjs"
+	claims["aud"] = "billing.jwtgo.io"
+	claims["iss"] = "jwtgo.io"
+	claims["exp"] = time.Now().Add(time.Minute * 1).Unix()
+
+	tokenstring, err := token.SignedString(MySigningKey)
+	if err != nil {
+		fmt.Errorf("Something Went Wrong")
+		return "", err
+	}
+	return tokenstring, nil
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	validToken, err := GetJWT()
+	fmt.Println(validToken)
+	if err != nil {
+		fmt.Println("Failed to generate the token")
+	}
+	fmt.Fprintf(w, string(validToken))
+}
+
+func handleRequest() {
+	http.HandleFunc("/", Index)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func main() {
+	handleRequest()
+
+}
