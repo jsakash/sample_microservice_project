4c0570f129e184a4c5e6715314d2ed9b8d234ac4
Akash JS <akashjs710@gmail.com>
2022-11-15 17:49:06 +0530

    jwt creation complete

--- api/main.go
+++ api/main.go
@@ -5,6 +5,8 @@ import (
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/dgrijalva/jwt-go"
 )
 
 var MySigningKey = []byte(os.Getenv("SECRET_KEY"))
@@ -17,8 +19,33 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 func isAuthorized(endpoint func(http.ResponseWriter, *http.Request)) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.Header["Token"] != nil {
+			token, err := jwt.Parse(r.Header["Token"][0], func(token *jwt.Token) (interface{}, error) {
+				if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+					return nil, fmt.Errorf(("invalid signing method"))
+				}
+				aud := "billing.jwtgo.io"
+				checkAudience := token.Claims.(jwt.MapClaims).VerifyAudience(aud, false)
+				if !checkAudience {
+					return nil, fmt.Errorf(("invalid audience"))
+				}
+				iss := "jwtgo.io"
+				checkIss := token.Claims.(jwt.MapClaims).VerifyIssuer(iss, false)
+				if !checkIss {
+					return nil, fmt.Errorf(("invalid issuer"))
+				}
+				return MySigningKey, nil
+			})
+			if err != nil {
+				fmt.Fprintf(w, err.Error())
+			}
 
+			if token.Valid {
+				endpoint(w, r)
+			}
+		} else {
+			fmt.Fprintf(w, "No a")
 		}
+
 	})
 }
 
